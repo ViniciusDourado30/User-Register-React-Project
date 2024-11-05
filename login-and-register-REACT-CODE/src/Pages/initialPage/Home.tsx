466bb08ae81f247fd4cdd2f118e7cf03b2237f78
Vinicius Dourado <viniciusdourado30@outlook.com>
2024-11-05 19:04:49 -0300

    Create initial page component

---
+++ login-and-register-REACT-CODE/src/Pages/initialPage/Home.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return(
+        <>
+        <div className="w-full h-screen bg-soundBackground bg-cover bg-no-repeat"></div>
+        </>
+    )
+}
+
+export default HomePage
