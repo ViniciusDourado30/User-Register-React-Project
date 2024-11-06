96a2815b6a5bff3c955b1f7750a6837ec2c4beca
Vinicius Dourado <viniciusdourado30@outlook.com>
2024-11-06 02:06:38 -0300

    Add component form to home page

--- login-and-register-REACT-CODE/src/Pages/initialPage/Home.tsx
+++ login-and-register-REACT-CODE/src/Pages/initialPage/Home.tsx
@@ -1,10 +1,13 @@
+import BlogForm from "@/components/inputForm/inputForm"
+
 const HomePage = () => {
 
     return (
         <>
             <div className="w-full h-screen bg-soundBackground bg-cover bg-no-repeat flex justify-center items-center">
-                <div className="w-1/3 h-2/3 bg-black rounded-xl backdrop-blur-sm flex justify-center items-center flex-col">
+                <div className="w-1/3 h-2/3 bg-black rounded-xl backdrop-blur-sm flex justify-center items-center flex-col gap-10">
                         <h2 className="text-ghostWhite font-bold text-2xl uppercase">Login</h2>
+                        <BlogForm/>
                 </div>
             </div>
         </>
