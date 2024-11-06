e234ea28c0a2682a39ba92d1ebf38098dcf88b9b
ViniciusDourado <viniciusdourado30@outlook.com>
2024-11-05 22:41:18 -0300

    Create form box

--- login-and-register-REACT-CODE/src/Pages/initialPage/Home.tsx
+++ login-and-register-REACT-CODE/src/Pages/initialPage/Home.tsx
@@ -1,7 +1,12 @@
 const HomePage = () => {
-    return(
+
+    return (
         <>
-        <div className="w-full h-screen bg-soundBackground bg-cover bg-no-repeat"></div>
+            <div className="w-full h-screen bg-soundBackground bg-cover bg-no-repeat flex justify-center items-center">
+                <div className="w-1/3 h-2/3 bg-black rounded-xl backdrop-blur-sm flex justify-center items-center flex-col">
+                        <h2 className="text-ghostWhite font-bold text-2xl uppercase">Login</h2>
+                </div>
+            </div>
         </>
     )
 }
