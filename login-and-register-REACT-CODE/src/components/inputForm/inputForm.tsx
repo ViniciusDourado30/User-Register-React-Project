8b4f1b0fc702a505efc887afc43d002b10d16445
Vinicius Dourado <viniciusdourado30@outlook.com>
2024-11-06 02:05:54 -0300

    Add input element

--- login-and-register-REACT-CODE/src/components/inputForm/inputForm.tsx
+++ login-and-register-REACT-CODE/src/components/inputForm/inputForm.tsx
@@ -1,30 +1,100 @@
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
 const BlogForm = () => {
-    
-    const formSchema = z.object({
-        username: z.string().min(5, {message: "O nome deve conter no mínimo 5 caracteres"}).max(20, {message: "O nome deve conter no máximo 20 caracteres"}),
-        email: z.string().email(),
-        password: z.string().min(8, {message: "O nome deve conter no mínimo 8 caracteres"}).max(20, {message: "O nome deve conter no máximo 20 caracteres"}),
-        confirmPassword: z.string().min(8, {message: "O nome deve conter no mínimo 8 caracteres"}).max(20, {message: "O nome deve conter no máximo 20 caracteres"})
-    })
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-          username: "",
-          email: "",
-          password: "",
-          confirmPassword: "",
-        },
-      })
+  const formSchema = z.object({
+    username: z.string().min(5, { message: "O nome deve conter no mínimo 5 caracteres" }).max(20, { message: "O nome deve conter no máximo 20 caracteres" }),
+    email: z.string().email(),
+    password: z.string().min(8, { message: "O nome deve conter no mínimo 8 caracteres" }).max(20, { message: "O nome deve conter no máximo 20 caracteres" }),
+    confirmPassword: z.string().min(8, { message: "O nome deve conter no mínimo 8 caracteres" }).max(20, { message: "O nome deve conter no máximo 20 caracteres" })
+  })
 
-      function onSubmit(values: z.infer<typeof formSchema>) {
-        
-        console.log(values)
-      }
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+
+    console.log(values)
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Username" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Senha" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Confirme a Senha" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">ENTRAR</Button>
+          <p className="text-ghostWhite">Não possui cadastro, <a className="text-red" href="">Clique Aqui</a></p>
+        </form>
+      </Form>
+    </>
+  )
 }
 
 export default BlogForm
